e6334c1f77dffe9d2125c6fc166acc268654bc2d
wezdez <westonbross@yahoo.com>
2016-01-19 15:57:25 -0800

    study drill

--- FirstProg.java
+++ FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
 	public static void main( String[] args) {
-		System.out.println( "I am determined to learn how to code." );
-		System.out.println( "Today's date is");
+		System.out.println( "I, Weston Ross am determined to learn how to code." );
+		System.out.println( "Today's date is Tuesday, January 19, 2015");
 	}
 }
