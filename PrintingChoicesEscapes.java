ac488025c61a5bc3b80a3f9c80c0f0be636512fe
wezdez <westonbross@yahoo.com>
2016-01-20 12:47:25 -0800

    ex: 4 + drill

---
+++ PrintingChoicesEscapes.java
@@ -0,0 +1,21 @@
+public class PrintingChoicesEscapes{
+	public static void main( String[] args){
+		System.out.println("Alpha");
+		System.out.println("Bravo");
+
+		System.out.println("Charlie");
+		System.out.println("Delta\n");
+
+		System.out.print("Echo");
+		System.out.print("Foxtrot");
+
+		System.out.println("Golf");
+		System.out.print("Hotel");
+		System.out.println("");
+		System.out.println("India\n");
+
+		System.out.println( "This" + " " + "is" + " " + "a" + " test." );
+		System.out.println("I am" + " " + "learning java" + " " + "the hard way.");
+
+	}
+}
