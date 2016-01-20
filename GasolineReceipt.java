f7b32664fbe3ebe9b3649e0a9de76622c5a27ba2
wezdez <westonbross@yahoo.com>
2016-01-19 16:27:41 -0800

    ex 2 with study drill

--- GasolineReceipt.java
+++ GasolineReceipt.java
@@ -1,16 +1,18 @@
 public class GasolineReceipt {
 	public static void main( String[] args){
 		System.out.println( "+-------------------------+");
-		System.out.println( "|                         |");
-		System.out.println( "|      CORNER STORE       |");
-		System.out.println( "|                         |");
-		System.out.println( "|   2016-19-01 03:38PM    |");
-		System.out.println( "|                         |");
-		System.out.println( "|  Gallons:        10.870 |");
-		System.out.println( "|  Price/gallon:  $ 2.089 |");
-		System.out.println( "|                         |");
-		System.out.println( "|  Fuel total:    $22.71  |");
-		System.out.println( "|                         |");
+		System.out.println( "|   |                  |   |");
+		System.out.println( "|   |   CORNER STORE   |   |");
+		System.out.println( "|  -|-                -|-  |");
+		System.out.println( "|   |                  |   |");
+		System.out.println( "|                          |");
+		System.out.println( "|    2016-19-01 03:38PM    |");
+		System.out.println( "|                          |");
+		System.out.println( "|  Gallons:         2.000  |");
+		System.out.println( "|  Price/gallon:  $ 3.330  |");
+		System.out.println( "|                          |");
+		System.out.println( "|  Fuel total:    $6.66    |");
+		System.out.println( "|                          |");
 		System.out.println( "+-------------------------+");
 	}
 
