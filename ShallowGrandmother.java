f36bbeb6470eef6add3f9da95dd6ce7a3076d459
wezdez <westonbross@yahoo.com>
2016-01-22 14:23:27 -0800

    ex:14 + drill

---
+++ ShallowGrandmother.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ShallowGrandmother{
+	public static void main( String[] args){
+		Scanner keyboard = new Scanner(System.in);
+		int age;
+		double income, cute, happy;
+		boolean allowed, allowed2;
+
+		System.out.print( "Enter your age: ");
+		age = keyboard.nextInt();
+
+		System.out.print( "Enter your yearly income: ");
+		income = keyboard.nextDouble();
+
+		System.out.print( "How cute are you, on a scale from 0.0 to 10.0? " );
+		cute = keyboard.nextDouble();
+
+
+		allowed = ( age > 25 && age < 40 && ( income > 50000 || cute >= 8.5  ) );
+ 		
+
+
+ 		System.out.println( "\nAllowed to date my granchild? " + allowed );
+
+ 		System.out.print( "\nBut wait...\nHow happy do you make them, on a scale from 0.0 to 10.0? ");
+ 		happy = keyboard.nextDouble();
+
+ 		allowed2 = (happy > 7 && ( age > 25 && age < 40 ) );
+
+ 		System.out.println( "Allowed to date my grandchild? " + allowed );
+	}
+
+}
