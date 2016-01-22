abb482b13989a2c57b309b322d983c746a057ac3
wezdez <westonbross@yahoo.com>
2016-01-22 15:46:59 -0800

    ex:17 + drill

---
+++ ClubBouncer.java
@@ -0,0 +1,18 @@
+public class ClubBouncer {
+	public static void main( String[] args ) {
+		int age = 22;
+		boolean onGuestList = false;
+		double allure = 7.5;
+		String gender = "F";
+
+		if ( onGuestList || age >= 21 || (gender.equals("F") && allure >= 8) ) {
+			System.out.println("You are allowed to enter the club.");
+		} System.out.println("er mer gerd");
+		else {
+			System.out.println("You are not allowed to enter the club.");
+		}
+	}
+}
+
+/*Drill answers:
+	this will not compile with the insertion between the if statement body and the else 
