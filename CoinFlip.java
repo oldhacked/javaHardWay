9105b5ba5afb297afe3ef8c1a8f032792027ec3f
wezdez <westonbross@yahoo.com>
2016-01-25 12:08:09 -0800

    ex:29 + drill

---
+++ CoinFlip.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class CoinFlip {
+	public static void main( String[] args ) {
+		Scanner keyboard = new Scanner(System.in);
+		String coin, again;
+		int streak = 0;
+		boolean gotHeads;
+
+		do {
+			gotHeads = Math.random() < 0.5;
+
+			if ( gotHeads ){
+				coin = "HEADS";
+			}
+			else{
+				coin = "TAILS";
+			}
+
+			System.out.println("You flip a coin and it is... " + coin);
+
+			if ( gotHeads ) {
+				streak++;
+				System.out.println("\tThat's " + streak + " in a row....");
+				System.out.print("\tWould you like to flip again (y/n)? ");
+				again = keyboard.next();
+			}
+			else {
+				System.out.println("\tYou lose everything!");
+				System.out.println("\tShould've quit while you were aHEAD....");
+				streak = 0;
+				again = "n";
+			}
+		} while ( again.equals("y") );
+
+		System.out.println( "Final score: " + streak );
+	}
+}
+
+
+/* Drill:
+	if this were to be converted into a while loop, the while condition would be placed before the body instead of do.
+	the String "again" then needs to be defined to y so that the body of the loop can start 
+*/
